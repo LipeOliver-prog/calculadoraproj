f2fda2beff3955a253eca722a11872b0dab4bc9f
User <User@DESKTOP-HUTMNCV>
2024-11-06 18:46:50 -0300

    Adicionar arquivos de projeto.

---
+++ calculadoraproj/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace calculadoraproj
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            this.btnzero = new System.Windows.Forms.Button();
+            this.btnquatro = new System.Windows.Forms.Button();
+            this.btnsete = new System.Windows.Forms.Button();
+            this.btncinco = new System.Windows.Forms.Button();
+            this.btnseis = new System.Windows.Forms.Button();
+            this.btnum = new System.Windows.Forms.Button();
+            this.btndois = new System.Windows.Forms.Button();
+            this.btntres = new System.Windows.Forms.Button();
+            this.btnnove = new System.Windows.Forms.Button();
+            this.btnoito = new System.Windows.Forms.Button();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.btnvirgula = new System.Windows.Forms.Button();
+            this.btnigual = new System.Windows.Forms.Button();
+            this.btnmenos = new System.Windows.Forms.Button();
+            this.btnmais = new System.Windows.Forms.Button();
+            this.btnx = new System.Windows.Forms.Button();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnzero
+            // 
+            this.btnzero.BackColor = System.Drawing.Color.DarkRed;
+            this.btnzero.ForeColor = System.Drawing.Color.Transparent;
+            this.btnzero.Location = new System.Drawing.Point(66, 240);
+            this.btnzero.Name = "btnzero";
+            this.btnzero.Size = new System.Drawing.Size(49, 42);
+            this.btnzero.TabIndex = 0;
+            this.btnzero.Text = "0";
+            this.btnzero.UseVisualStyleBackColor = false;
+            this.btnzero.Click += new System.EventHandler(this.btnzero_Click);
+            // 
+            // btnquatro
+            // 
+            this.btnquatro.BackColor = System.Drawing.Color.DarkRed;
+            this.btnquatro.ForeColor = System.Drawing.Color.Transparent;
+            this.btnquatro.Location = new System.Drawing.Point(11, 132);
+            this.btnquatro.Name = "btnquatro";
+            this.btnquatro.Size = new System.Drawing.Size(49, 42);
+            this.btnquatro.TabIndex = 1;
+            this.btnquatro.Text = "4";
+            this.btnquatro.UseVisualStyleBackColor = false;
+            this.btnquatro.Click += new System.EventHandler(this.btnquatro_Click);
+            // 
+            // btnsete
+            // 
+            this.btnsete.BackColor = System.Drawing.Color.DarkRed;
+            this.btnsete.ForeColor = System.Drawing.Color.Transparent;
+            this.btnsete.Location = new System.Drawing.Point(11, 78);
+            this.btnsete.Name = "btnsete";
+            this.btnsete.Size = new System.Drawing.Size(49, 42);
+            this.btnsete.TabIndex = 2;
+            this.btnsete.Text = "7";
+            this.btnsete.UseVisualStyleBackColor = false;
+            this.btnsete.Click += new System.EventHandler(this.btnsete_Click);
+            // 
+            // btncinco
+            // 
+            this.btncinco.BackColor = System.Drawing.Color.DarkRed;
+            this.btncinco.ForeColor = System.Drawing.Color.Transparent;
+            this.btncinco.Location = new System.Drawing.Point(66, 132);
+            this.btncinco.Name = "btncinco";
+            this.btncinco.Size = new System.Drawing.Size(49, 42);
+            this.btncinco.TabIndex = 3;
+            this.btncinco.Text = "5";
+            this.btncinco.UseVisualStyleBackColor = false;
+            this.btncinco.Click += new System.EventHandler(this.btncinco_Click);
+            // 
+            // btnseis
+            // 
+            this.btnseis.BackColor = System.Drawing.Color.DarkRed;
+            this.btnseis.ForeColor = System.Drawing.Color.Transparent;
+            this.btnseis.Location = new System.Drawing.Point(121, 132);
+            this.btnseis.Name = "btnseis";
+            this.btnseis.Size = new System.Drawing.Size(49, 42);
+            this.btnseis.TabIndex = 4;
+            this.btnseis.Text = "6";
+            this.btnseis.UseVisualStyleBackColor = false;
+            this.btnseis.Click += new System.EventHandler(this.btnseis_Click);
+            // 
+            // btnum
+            // 
+            this.btnum.BackColor = System.Drawing.Color.Maroon;
+            this.btnum.ForeColor = System.Drawing.Color.Transparent;
+            this.btnum.Location = new System.Drawing.Point(11, 186);
+            this.btnum.Name = "btnum";
+            this.btnum.Size = new System.Drawing.Size(49, 42);
+            this.btnum.TabIndex = 5;
+            this.btnum.Text = "1";
+            this.btnum.UseVisualStyleBackColor = false;
+            this.btnum.Click += new System.EventHandler(this.btnum_Click);
+            // 
+            // btndois
+            // 
+            this.btndois.BackColor = System.Drawing.Color.DarkRed;
+            this.btndois.ForeColor = System.Drawing.Color.Transparent;
+            this.btndois.Location = new System.Drawing.Point(66, 186);
+            this.btndois.Name = "btndois";
+            this.btndois.Size = new System.Drawing.Size(49, 42);
+            this.btndois.TabIndex = 6;
+            this.btndois.Text = "2";
+            this.btndois.UseVisualStyleBackColor = false;
+            this.btndois.Click += new System.EventHandler(this.btndois_Click);
+            // 
+            // btntres
+            // 
+            this.btntres.BackColor = System.Drawing.Color.DarkRed;
+            this.btntres.ForeColor = System.Drawing.Color.Transparent;
+            this.btntres.Location = new System.Drawing.Point(121, 186);
+            this.btntres.Name = "btntres";
+            this.btntres.Size = new System.Drawing.Size(49, 42);
+            this.btntres.TabIndex = 7;
+            this.btntres.Text = "3";
+            this.btntres.UseVisualStyleBackColor = false;
+            this.btntres.Click += new System.EventHandler(this.btntres_Click);
+            // 
+            // btnnove
+            // 
+            this.btnnove.BackColor = System.Drawing.Color.DarkRed;
+            this.btnnove.ForeColor = System.Drawing.Color.Transparent;
+            this.btnnove.Location = new System.Drawing.Point(121, 78);
+            this.btnnove.Name = "btnnove";
+            this.btnnove.Size = new System.Drawing.Size(49, 42);
+            this.btnnove.TabIndex = 8;
+            this.btnnove.Text = "9";
+            this.btnnove.UseVisualStyleBackColor = false;
+            this.btnnove.Click += new System.EventHandler(this.btnnove_Click);
+            // 
+            // btnoito
+            // 
+            this.btnoito.BackColor = System.Drawing.Color.DarkRed;
+            this.btnoito.ForeColor = System.Drawing.Color.Transparent;
+            this.btnoito.Location = new System.Drawing.Point(66, 78);
+            this.btnoito.Name = "btnoito";
+            this.btnoito.Size = new System.Drawing.Size(49, 42);
+            this.btnoito.TabIndex = 9;
+            this.btnoito.Text = "8";
+            this.btnoito.UseVisualStyleBackColor = false;
+            this.btnoito.Click += new System.EventHandler(this.btnoito_Click);
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.BackColor = System.Drawing.Color.Brown;
+            this.txtresultado.ForeColor = System.Drawing.Color.Transparent;
+            this.txtresultado.Location = new System.Drawing.Point(27, 10);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.ReadOnly = true;
+            this.txtresultado.Size = new System.Drawing.Size(177, 20);
+            this.txtresultado.TabIndex = 10;
+            this.txtresultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtresultado.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnvirgula
+            // 
+            this.btnvirgula.BackColor = System.Drawing.Color.DarkRed;
+            this.btnvirgula.ForeColor = System.Drawing.Color.Transparent;
+            this.btnvirgula.Location = new System.Drawing.Point(11, 241);
+            this.btnvirgula.Name = "btnvirgula";
+            this.btnvirgula.Size = new System.Drawing.Size(49, 42);
+            this.btnvirgula.TabIndex = 11;
+            this.btnvirgula.Text = ".";
+            this.btnvirgula.UseVisualStyleBackColor = false;
+            this.btnvirgula.Click += new System.EventHandler(this.btnvirgula_Click);
+            // 
+            // btnigual
+            // 
+            this.btnigual.BackColor = System.Drawing.Color.DarkRed;
+            this.btnigual.ForeColor = System.Drawing.Color.Transparent;
+            this.btnigual.Location = new System.Drawing.Point(121, 240);
+            this.btnigual.Name = "btnigual";
+            this.btnigual.Size = new System.Drawing.Size(49, 42);
+            this.btnigual.TabIndex = 12;
+            this.btnigual.Text = "=";
+            this.btnigual.UseVisualStyleBackColor = false;
+            this.btnigual.Click += new System.EventHandler(this.btnigual_Click);
+            // 
+            // btnmenos
+            // 
+            this.btnmenos.BackColor = System.Drawing.Color.DarkRed;
+            this.btnmenos.ForeColor = System.Drawing.Color.Transparent;
+            this.btnmenos.Location = new System.Drawing.Point(176, 186);
+            this.btnmenos.Name = "btnmenos";
+            this.btnmenos.Size = new System.Drawing.Size(49, 42);
+            this.btnmenos.TabIndex = 13;
+            this.btnmenos.Text = "-";
+            this.btnmenos.UseVisualStyleBackColor = false;
+            this.btnmenos.Click += new System.EventHandler(this.btnmenos_Click);
+            // 
+            // btnmais
+            // 
+            this.btnmais.BackColor = System.Drawing.Color.DarkRed;
+            this.btnmais.ForeColor = System.Drawing.Color.Transparent;
+            this.btnmais.Location = new System.Drawing.Point(174, 240);
+            this.btnmais.Name = "btnmais";
+            this.btnmais.Size = new System.Drawing.Size(49, 42);
+            this.btnmais.TabIndex = 14;
+            this.btnmais.Text = "+";
+            this.btnmais.UseVisualStyleBackColor = false;
+            this.btnmais.Click += new System.EventHandler(this.btnmais_Click);
+            // 
+            // btnx
+            // 
+            this.btnx.BackColor = System.Drawing.Color.DarkRed;
+            this.btnx.ForeColor = System.Drawing.Color.Transparent;
+            this.btnx.Location = new System.Drawing.Point(174, 132);
+            this.btnx.Name = "btnx";
+            this.btnx.Size = new System.Drawing.Size(49, 42);
+            this.btnx.TabIndex = 15;
+            this.btnx.Text = "x";
+            this.btnx.UseVisualStyleBackColor = false;
+            this.btnx.Click += new System.EventHandler(this.btnx_Click);
+            // 
+            // btndiv
+            // 
+            this.btndiv.BackColor = System.Drawing.Color.DarkRed;
+            this.btndiv.ForeColor = System.Drawing.Color.Transparent;
+            this.btndiv.Location = new System.Drawing.Point(174, 79);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(49, 41);
+            this.btndiv.TabIndex = 16;
+            this.btndiv.Text = "/";
+            this.btndiv.UseVisualStyleBackColor = false;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.DarkOrange;
+            this.button17.ForeColor = System.Drawing.Color.Transparent;
+            this.button17.Location = new System.Drawing.Point(174, 46);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(49, 21);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "c";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.Red;
+            this.lbl1.ForeColor = System.Drawing.Color.Transparent;
+            this.lbl1.Location = new System.Drawing.Point(29, 13);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 18;
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lbl1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-395, -167);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(684, 484);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 295);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.btnx);
+            this.Controls.Add(this.btnmais);
+            this.Controls.Add(this.btnmenos);
+            this.Controls.Add(this.btnigual);
+            this.Controls.Add(this.btnvirgula);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.btnoito);
+            this.Controls.Add(this.btnnove);
+            this.Controls.Add(this.btntres);
+            this.Controls.Add(this.btndois);
+            this.Controls.Add(this.btnum);
+            this.Controls.Add(this.btnseis);
+            this.Controls.Add(this.btncinco);
+            this.Controls.Add(this.btnsete);
+            this.Controls.Add(this.btnquatro);
+            this.Controls.Add(this.btnzero);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Calculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Calculadora_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnzero;
+        private System.Windows.Forms.Button btnquatro;
+        private System.Windows.Forms.Button btnsete;
+        private System.Windows.Forms.Button btncinco;
+        private System.Windows.Forms.Button btnseis;
+        private System.Windows.Forms.Button btnum;
+        private System.Windows.Forms.Button btndois;
+        private System.Windows.Forms.Button btntres;
+        private System.Windows.Forms.Button btnnove;
+        private System.Windows.Forms.Button btnoito;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Button btnvirgula;
+        private System.Windows.Forms.Button btnigual;
+        private System.Windows.Forms.Button btnmenos;
+        private System.Windows.Forms.Button btnmais;
+        private System.Windows.Forms.Button btnx;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ calculadoraproj/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadoraproj
+{
+    
+
+    public partial class Calculadora : Form
+
+    {
+       
+
+        public decimal Resultado {  get; set; }
+        public decimal Valor { get; set; }
+
+
+        private Operaçao OperaçaoSelecionada { get; set; }
+
+        private enum Operaçao
+        {
+            Adiçao,
+            Subtraçao,
+            Multiplicaçao,
+            Divisao
+        }
+
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Calculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
+
+        private void btnzero_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "0";
+
+        }
+
+        private void btndois_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "2";
+        }
+
+        private void btntres_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "3";
+        }
+
+        private void btncinco_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "5";
+        }
+
+        private void btnseis_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "6";
+        }
+
+        private void btnsete_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "7";
+        }
+
+        private void btnoito_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "8";
+        }
+
+        private void btnnove_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "9";
+        }
+
+        private void btnum_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "1";
+        }
+
+        private void btnquatro_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text += "4";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmais_Click(object sender, EventArgs e)
+        {
+            OperaçaoSelecionada = Operaçao.Adiçao;
+            Valor = Convert.ToDecimal(txtresultado.Text);
+            txtresultado.Text= "";
+            lbl1.Text = "+";
+        }
+
+        private void btnmenos_Click(object sender, EventArgs e)
+        {
+            OperaçaoSelecionada = Operaçao.Subtraçao;
+            Valor = Convert.ToDecimal(txtresultado.Text);
+            txtresultado.Text = "";
+            lbl1.Text = "-";
+        }
+
+        private void btnx_Click(object sender, EventArgs e)
+        {
+            OperaçaoSelecionada = Operaçao.Multiplicaçao;
+            Valor = Convert.ToDecimal(txtresultado.Text);
+            txtresultado.Text = "";
+            lbl1.Text = "*";
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            OperaçaoSelecionada = Operaçao.Divisao;
+            Valor = Convert.ToDecimal(txtresultado.Text);
+            txtresultado.Text = "";
+            lbl1.Text = "/";
+        }
+
+        private void btnigual_Click(object sender, EventArgs e)
+        {
+            switch (OperaçaoSelecionada)
+            {
+                case Operaçao.Adiçao:
+                    Resultado = Valor + Convert.ToDecimal(txtresultado.Text);   
+                    break;
+                case Operaçao.Subtraçao:
+                    Resultado = Valor - Convert.ToDecimal(txtresultado.Text);
+                    break;
+                case Operaçao.Multiplicaçao:
+                    Resultado = Valor * Convert.ToDecimal(txtresultado.Text);
+                    break;
+                case Operaçao.Divisao:
+                    Resultado = Valor / Convert.ToDecimal(txtresultado.Text);
+                    break;
+               
+            }
+         txtresultado.Text =Convert.ToString(Resultado);
+        }
+
+        private void btnvirgula_Click(object sender, EventArgs e)
+        {
+            if (!txtresultado.Text.Contains(","))
+            txtresultado.Text += ",";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            txtresultado.Text = "";
+            lbl1.Text = "";
+        }
+    }
+
+
+}
